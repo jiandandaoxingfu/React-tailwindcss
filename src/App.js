9eb7ec387a3bc4f2dc1037ce41015f1820648d09
JMx <33108686+jiandandaoxingfu@users.noreply.github.com>
2022-11-17 20:36:24 +0800

    add:  weixin web page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
   <div>
+
     <div className="container flex flex-row h-screen w-screen mx-auto border-2 rounded-xl overflow-hidden">
       <div className="flex flex-col h-screen w-1/5 min-w-max bg-gray-600">
         <a href="https://github.com/upscayl" target="_blank">
           <div class="container mx-auto flex items-center space-x-4 p-3 mb-4">
-            <img class="h-12 w-12 shrink-0 rounded-3xl" src="https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=80" />
+            <img class="h-12 w-12 shrink-0 rounded-3xl" src="https://avatars.githubusercontent.com/u/112148288?s=200&v=4" />
             <div className="">
               <span className="font-black text-3xl text-white">Upscayl</span><br/>
               <span className="text-gray-400 font-medium">Technical advisor</span>
@@ -69,15 +69,96 @@ function App() {
             </a>
           </p>
         </div>
-
       </div>
 
-      <div className="h-screen w-screen bg-gray-900">
+      <div className="h-screen w-screen bg-gray-900 overflow-y-auto">
+        <div className="w-full flex flex-col items-center bg-green-500 overflow-hidden">
+          <p className="text-white font-medium text-3xl text-center mt-4">微信</p>
+          
+            <div className="container mx-auto w-96 my-12">
+          <hr className="border-1 mt-12 mb-4"/>      
+          <ul className="space-y-2 text-white">
+            <li>
+              <a href="#">微信 8.0.30 for iOS 正式版发布</a>
+              <span className="float-right">11-14</span>
+            </li>
+            <li>
+              <a href="#">微信 8.0.30 for iOS 正式版发布</a>
+              <span className="float-right">11-14</span>
+            </li>
+            <li>
+              <a href="#">微信 8.0.30 for iOS 正式版发布</a>
+              <span className="float-right">11-14</span>
+            </li>
+            <li>
+              <a href="#">微信 8.0.30 for iOS 正式版发布</a>
+              <span className="float-right">11-14</span>
+            </li>
+            <li className="py-2">
+              <a href="#">更多 > </a>
+            </li>
+          </ul>
+          </div>  
+  
+            <div className="flex flex-wrap text-center gap-4 mx-10">
+          <div className="pt-10 mx-auto rounded-full h-28 w-28 bg-blue-500/10 hover:bg-blue-500/30 animate">
+            <span className="">Android</span>
+          </div>
+          <div className="pt-10 mx-auto rounded-full h-28 w-28 bg-blue-500/10 hover:bg-blue-500/30 animate">
+            <span className="">iOS</span>
+          </div>
+          <div className="pt-10 mx-auto rounded-full h-28 w-28 bg-blue-500/10 hover:bg-blue-500/30 animate">
+            <span className="">Windows</span>
+          </div>
+          <div className="pt-10 mx-auto rounded-full h-28 w-28 bg-blue-500/10 hover:bg-blue-500/30 animate">
+            <span className="">macOS</span>
+          </div>
+          <div className="pt-10 mx-auto rounded-full h-28 w-28 bg-blue-500/10 hover:bg-blue-500/30 animate">
+            <span className="">文件传输助手网页版</span>
+          </div>
+          </div>  
+  
+            <div class="my-20 flex flex-wrap text-center gap-4 mx-10">
+          <div class="mx-auto w-48 h-32 rounded-xl bg-white hover:scale-105 animate">
+           
+          </div>
+          <div class="mx-auto w-48 h-32 rounded-xl bg-white hover:scale-105 animate">
+        
+          </div>
+          <div class="mx-auto w-48 h-32 rounded-xl bg-white hover:scale-105 animate">
+        
+          </div>
+          <div class="mx-auto w-48 h-32 rounded-xl bg-white hover:scale-105 animate">
+        
+          </div>
+          <div class="mx-auto w-48 h-36 rounded-xl bg-white hover:scale-105 animate">
+        
+          </div>
+          <div class="mx-auto w-48 h-36 rounded-xl bg-white hover:scale-105 animate">
+        
+          </div>
+          <div class="mx-auto w-48 h-36 rounded-xl bg-white hover:scale-105 animate">
+        
+          </div>
+          <div class="mx-auto w-48 h-36 rounded-xl bg-white hover:scale-105 animate">
+        
+          </div>
+          <div class="mx-auto w-48 h-36 rounded-xl bg-white hover:scale-105 animate">
+        
+          </div>
+          <div class="mx-auto w-48 h-36 rounded-xl bg-white hover:scale-105 animate">
+        
+          </div>
+          <div class="mx-auto w-48 h-36 rounded-xl bg-white hover:scale-105 animate">
+        
+          </div>
+          <div class="mx-auto w-48 h-36 rounded-xl bg-white hover:scale-105 animate">
+        
+          </div>
+          </div>
+        </div>
       </div>
     </div>
-
-    
-
   </div>
   );
 }
