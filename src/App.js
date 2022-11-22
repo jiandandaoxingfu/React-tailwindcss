1c29dcbfbaa3c07c2aa8faf41ff299d48033ac84
JMx <33108686+jiandandaoxingfu@users.noreply.github.com>
2022-11-22 20:42:26 +0800

    Update App.js
    学习flex布局

--- src/App.js
+++ src/App.js
@@ -65,7 +65,8 @@ function App() {
     <div>
       {/* Upscayl */}
       <div className="container flex flex-row h-screen w-screen mx-auto border-2 rounded-xl overflow-hidden">
-        <div className="flex flex-col h-screen w-1/5 min-w-max bg-gray-600">
+        {/*LeftPanel*/}
+        <div className="flex flex-col h-full w-1/5 min-w-max bg-gray-600 justify-between">
           {/* Header */}
           <a href="https://github.com/upscayl">
             <div className="container mx-auto flex items-center space-x-4 p-3 mb-4">
@@ -78,7 +79,7 @@ function App() {
           </a>
 
           {/* Menu */}
-          <div className="ml-3 overflow-y-auto space-y-6">
+          <div className="ml-3 overflow-y-auto space-y-6 flex-grow">
             {
               new Array(12).join(',').split(',').map((i, j) =>
                 <>
@@ -214,7 +215,7 @@ function App() {
           </div>
         </div>
 
-        <div className="alert alert-success shadow-lg">
+        <div className="alert alert-success shadow-lg w-2/3">
           <div>
             <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
             <span>Your purchase has been confirmed!</span>
@@ -331,8 +332,8 @@ function App() {
               <div className="flex-1 px-2 mx-2">Navbar Title</div>
               <div className="flex-none hidden lg:block">
                 <ul className="menu menu-horizontal">
-                  <li><a>Navbar Item 1</a></li>
-                  <li><a>Navbar Item 2</a></li>
+                  <li><a href="/#">Navbar Item 1</a></li>
+                  <li><a href="/#">Navbar Item 2</a></li>
                 </ul>
               </div>
             </div>
@@ -340,8 +341,8 @@ function App() {
           <div className="drawer-side">
             <label htmlFor="my-drawer-3" className="drawer-overlay"></label> 
             <ul className="menu p-4 w-80 bg-base-100">
-              <li><a>Sidebar Item 1</a></li>
-              <li><a>Sidebar Item 2</a></li>
+              <li><a href="/#">Sidebar Item 1</a></li>
+              <li><a href="/#">Sidebar Item 2</a></li>
             </ul>
           </div>
         </div>
@@ -375,9 +376,9 @@ function App() {
         </div>
 
         <div className="tabs">
-          <a className="tab tab-lifted tab-active">Tab 1</a>
-          <a className="tab tab-lifted">Tab 2</a>
-          <a className="tab tab-lifted">Tab 3</a>
+          <a href="/#" className="tab tab-lifted tab-active">Tab 1</a>
+          <a href="/#" className="tab tab-lifted">Tab 2</a>
+          <a href="/#" className="tab tab-lifted">Tab 3</a>
         </div>
 
         <div className="w-full max-w-md px-2 sm:px-0">
@@ -419,7 +420,7 @@ function App() {
                           <li>{post.shareCount} shares</li>
                         </ul>
     
-                        <a className={classNames(
+                        <a href="/#" className={classNames(
                             'absolute inset-0 rounded-md',
                             'ring-blue-400 focus:z-10 focus:outline-none focus:ring-2'
                           )}>{post.id}</a>
@@ -479,7 +480,7 @@ function App() {
                           <li>{post.shareCount} shares</li>
                         </ul>
     
-                        <a className={classNames(
+                        <a href="/#" className={classNames(
                             'absolute inset-0 rounded-md',
                             'ring-blue-400 focus:z-10 focus:outline-none focus:ring-2'
                           )}>{post.id}</a>
@@ -500,16 +501,16 @@ function App() {
 
         <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
           <div className="grid grid-flow-col gap-4">
-            <a className="link link-hover">About us</a> 
-            <a className="link link-hover">Contact</a> 
-            <a className="link link-hover">Jobs</a> 
-            <a className="link link-hover">Press kit</a>
+            <a href="/#" className="link link-hover">About us</a> 
+            <a href="/#" className="link link-hover">Contact</a> 
+            <a href="/#" className="link link-hover">Jobs</a> 
+            <a href="/#" className="link link-hover">Press kit</a>
           </div> 
           <div>
             <div className="grid grid-flow-col gap-4">
-              <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a> 
-              <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a> 
-              <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+              <a href="/#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a> 
+              <a href="/#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a> 
+              <a href="/#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
             </div>
           </div> 
           <div>
